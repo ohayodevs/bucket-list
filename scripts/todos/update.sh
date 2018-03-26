d4f14fa1cc0184b921e44c3c27c99bc769e8326b
andrewinnocent <innocent.andrew@gmail.com>
2018-03-26 16:40:45 -0400

    Change the word item to name Worked with @lydivetteroldan

--- scripts/todos/update.sh
+++ scripts/todos/update.sh
@@ -10,7 +10,7 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "todo": {
-      "name": "'"${ITEM}"'",
+      "name": "'"${NAME}"'",
       "description": "'"${DESCRIPTION}"'",
       "date": "'"${DATE}"'",
       "location": "'"${LOCATION}"'"
